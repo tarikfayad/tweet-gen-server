a5e2ad00958b1171023ead584682c19fdae8b50a
Tarik Fayad <tarikfayad@gmail.com>
2021-10-31 23:55:09 -0400

    Added the ability to parse Hashtags from game name.

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const app = require('express')();
 const axios = require('axios');
 
-let gameName ='';
+let gameName = '';
+let tournamentName = '';
 
 const axiosAPI = axios.create({
   baseURL: process.env.CHALLONGE_BASE_URL
@@ -19,7 +20,7 @@ app.post('/tweet-gen', async (req, res) => {
     console.log('FIND ME!');
     console.log(req.body);
     const response = await axiosAPI.get('tournaments/' + req.body.organization + '-' + req.body.tournament_slug + '/matches.json?api_key=' + process.env.CHALLONGE_API_KEY);
-    return res.status(200).json(await parseMatches(response.data, req.body.button, req.body.service, req.body.organization, req.body.tournament_slug));
+    return res.status(200).json(await parseMatches(response.data, req.body));
   } catch (e) {
     console.log(e);
   }
@@ -55,21 +56,24 @@ app.listen(port, () => console.log(`Tweet app backend is running on port ${port}
 // Helper methods
 //Yes I know it's a little messy to pass along all of these variables,
 //but it's the path of least resistance to make sure that the correct info get's spit out.
-async function parseMatches(matches, button, service, organization, tournament) {
-  switch (button) {
+async function parseMatches(matches, body) {
+  switch (body.button) {
     case 'starting-soon':
       return [{
         'message': 'Boutta start in about 30 minutes! 💪\n\n[EMBED LATEST REMINDER TWEET]'
       }];
       break;
     case 'kickoff':
-      return [];
+      await isTournamentInProgress(body['service'], body['organization'], body['tournament_slug']);
+      return [{
+        'message': "Aaaand we're live with " + tournamentName + "!\n\n🎙️ @" + body.com1.replace("@", "") + " & @" + body.com2.replace("@", "") + " | @" + body.com3.replace("@", "") + " & @" + body.com4.replace("@", "") + "\n⚔️ " + body.bracket + "\n\n📺 https://twitch.tv/ImpurestClub\n💰 " + body.matcherino + "\n\n" + getHashtags(gameName)
+      }];
       break;
     case 'top-16':
       return [];
       break;
     case 'top-8':
-      if (await isTournamentInProgress(service, organization, tournament)) {
+      if (await isTournamentInProgress(body['service'], body['organization'], body['tournament_slug'])) {
         var winnersRound = parseInt(matches[matches.length-1]['match']['round']) - 2;
         var losersRound = parseInt(matches[matches.length-3]['match']['round']) + 3;
         var winners = findMatchesInRound(matches, winnersRound);
@@ -82,7 +86,7 @@ async function parseMatches(matches, button, service, organization, tournament) 
       }
       break;
     case 'top-4':
-      if (await isTournamentInProgress(service, organization, tournament)) {
+      if (await isTournamentInProgress(body['service'], body['organization'], body['tournament_slug'])) {
         var winnersFinalsRound = parseInt(matches[matches.length-1]['match']['round']) - 1;
         return findMatchesInRound(matches, winnersFinalsRound);
       } else {
@@ -92,7 +96,7 @@ async function parseMatches(matches, button, service, organization, tournament) 
       }
       break;
     case 'losers-semis':
-      if (await isTournamentInProgress(service, organization, tournament)) {
+      if (await isTournamentInProgress(body['service'], body['organization'], body['tournament_slug'])) {
         return [matches[matches.length-4]];
       } else {
         return [{
@@ -101,7 +105,7 @@ async function parseMatches(matches, button, service, organization, tournament) 
       }
       break;
     case 'losers-finals':
-      if (await isTournamentInProgress(service, organization, tournament)) {
+      if (await isTournamentInProgress(body['service'], body['organization'], body['tournament_slug'])) {
       } else {
         return [matches[matches.length-3]];
         return [{
@@ -110,7 +114,7 @@ async function parseMatches(matches, button, service, organization, tournament) 
       }
       break;
     case 'grand-finals':
-      if (await isTournamentInProgress(service, organization, tournament)) {
+      if (await isTournamentInProgress(body['service'], body['organization'], body['tournament_slug'])) {
       } else {
         return [matches[matches.length-2]];
         return [{
@@ -119,7 +123,7 @@ async function parseMatches(matches, button, service, organization, tournament) 
       }
       break;
     case 'reset':
-      if (await isTournamentInProgress(service, organization, tournament)) {
+      if (await isTournamentInProgress(body['service'], body['organization'], body['tournament_slug'])) {
       } else {
         return [matches[matches.length-1]];
         return [{
@@ -128,7 +132,7 @@ async function parseMatches(matches, button, service, organization, tournament) 
       }
       break;
     case 'results':
-      if (await isTournamentInProgress(service, organization, tournament)) {
+      if (await isTournamentInProgress(body['service'], body['organization'], body['tournament_slug'])) {
         return [{
           'error': '⚠️ This command only works if the bracket is COMPLETED.'
         }];
@@ -159,11 +163,30 @@ async function isTournamentInProgress(service, organization, tournament) {
   const response = await axiosAPI.get('tournaments/' + organization + '-' + tournament + '.json?api_key=' + process.env.CHALLONGE_API_KEY);
   var tournamentDictionary = response.data['tournament'];
   gameName = tournamentDictionary['game_name']; //Yes this is a janky place to set the game name.
+  tournamentName = tournamentDictionary['name'];
   if (tournamentDictionary['state'] === 'complete') {
     return false;
   } else {return true;}
 }
 
-function formatTweet() {
+async function getTwitterHandles(service, organization, tournament, matches) {
 
+}
+
+function getHashtags(game) {
+  switch (game) {
+    case 'Granblue Fantasy Versus':
+      return '#GBVS #GranblueFantasy'
+      break;
+    case 'Under Night In-Birth Exe:Late[cl-r]':
+      return '#UNICLR #inbirth'
+      break;
+    case 'Guilty Gear -Strive-':
+      return '#GGST #GuiltyGear'
+      break;
+    case 'Melty Blood: Type Lumina':
+      return '#MBTL #MBTL_Tournament'
+      break;
+    default:
+  }
 }
